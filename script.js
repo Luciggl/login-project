ee52b8ca50bcffeed0de16ae6bbd8c1408ead588
luciggl <lucielionascimento19@gmail.com>
2025-01-15 20:37:03 -0300

    feat: criando a validação da senha

--- script.js
+++ script.js
@@ -2,11 +2,18 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     event.preventDefault();
 
     const email = document.getElementById('email').value.trim();
+    const password = document.getElementById('password').value.trim();
     const errorDiv = document.getElementById('error');
 
     const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     if (!emailRegex.test(email)) {
         errorDiv.textContent = 'Por favor, insira um e-mail válido.';
+        errorDiv.style.color = 'red';
+        return;
+    }
+
+    if (password.length < 6) {
+        errorDiv.textContent = 'A senha deve ter pelo menos 6 caracteres.';
         errorDiv.style.color = 'red';
         return;
     }
